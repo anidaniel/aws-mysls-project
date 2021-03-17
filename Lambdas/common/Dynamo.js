1581d3bedd32190086c0c49b7e4da2afe5a763d7
Anidaniel <theanidaniel@gmail.com>
2021-03-17 14:39:02 +0530

    yml changes and dynam_response_fetch added.

--- Lambdas/common/Dynamo.js
+++ Lambdas/common/Dynamo.js
@@ -3,17 +3,26 @@ const aws = require("aws-sdk");
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
 const Dynamo = {
-    async get (ID, TableName) {
-        const params = {
-            TableName,
-            Key: {
-                ID 
-            }
-        };
+  async get(ID, TableName) {
+    const params = {
+      TableName,
+      Key: {
+        ID,
+      },
+    };
 
-        const data = await documentClient
-            .get(params)
-            .promise()
-        if (!data || !data.Item)
+    const data = await documentClient.get(params).promise();
+
+    if (!data || !data.Item) {
+      throw Error(
+        "There was an error fetching the data for ID of ${ID} from ${TableName}"
+      );
     }
-}
+
+    console.log(data);
+
+    return data.Item;
+  },
+};
+
+module.exports = Dynamo;
